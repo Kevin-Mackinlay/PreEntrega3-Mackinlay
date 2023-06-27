dc1c163aef94635e8c1781a1d119a86e64d36416
Mackinlay Kevin <kemack83@gmail.com>
2023-06-26 22:35:27 -0300

    primer clase

---
+++ index.js
@@ -0,0 +1,45 @@
+// tipos de variables
+
+// declara una variable
+
+var nombre;
+let apellido;
+
+// Agregar una variable
+
+nombre = "Dante";
+console.log(nombre);
+apellido = "Diaz";
+
+nombre = "Juan";
+
+// variable asignada
+
+// let edad = 40;
+
+// Const
+
+const pais = "Argentina";
+
+// Camel Case
+
+let kevinMackinlay = "programador javascript";
+
+
+// Salida de datos
+
+// console.log("Hola mundo");
+// console.log(apellido);
+// console.log("la edad es: " + edad);
+
+// alert("Hola");
+
+// let edad = prompt("Ingrese su edad");
+// alert("La edad ingresada es:"  + edad);
+
+//Ejercicio
+
+let añoDeNacimiento = parseInt(prompt("Ingrese su año de nacimiento"))
+let añoActual = 2023;
+let edadAprox = añoActual - añoDeNacimiento;
+console.log("tu edad aproximada es = " + edadAprox); 
