26525ab3a60500d7a8b307d18d10efed85a963e7
Mackinlay Kevin <kemack83@gmail.com>
2023-07-08 14:38:30 -0300

    Practica de Ciclos y condicionales

--- Js/miarchivo.js
+++ Js/miarchivo.js
@@ -1,11 +1,30 @@
-// let insertNumber = parseInt(prompt('Put a number'));
+//algoritmo con un condicional
 
-// for ( let number = 0; number < 10; number++) {
-//     console.log(number);
-// }
+alert('Bienvenido a la Vinoteca virtual!');
+let edad = parseInt(prompt('Ingrese su edad'));
+if(edad < 18){
+    alert('usted es menor de edad y no puede realizar la compra');
+}else{
+    alert('usted es mayor de edad y puede realizar la compra');
+}
+
+
+
+//algoritmo utilizando ciclo (while)
 
 let insertText = prompt("Put a text");
 while (insertText != "ESC") {
   alert("The user entered " + insertText);
   insertText = prompt("enter another text");
+}
+
+
+//algoritmo utilizando (for)
+
+alert('Bienvenido a los mejores masajes por orden de llegada')
+
+for(i = 1 ; i <= 4; i++) {
+   let insertNombre = prompt("ingrese un nombre")
+   alert('turno Numero ' + i +  ' nombre:  ' +   insertNombre );
+
 }
