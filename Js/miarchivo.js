a7e95226f2651f30a838438f0ee981e0bcf21a23
Mackinlay Kevin <kemack83@gmail.com>
2023-07-23 19:53:00 -0300

    aplicacion de funciones en el carrito de compras

--- Js/miarchivo.js
+++ Js/miarchivo.js
@@ -30,6 +30,52 @@
 
 alert("Bienvenido a la tienda FUNGI");
 
+class Producto {
+  constructor(id, nombre, precio, stock) {
+    this.id = id;
+    this.nombre = nombre;
+    this.precio = precio;
+    this.stock = stock;
+  }
+
+  // sumarProducto(precio) {
+  //   this.precio += precio;
+  // }
+}
+
+const mostrarProductos = (productos) => {
+  console.log("productos");
+
+  productos.forEach((producto) => console.log(producto));
+};
+
+const eliminarProducto = () => {
+  const productoCarrito = productoExistente();
+
+  if (!productoCarrito) return;
+
+  const confirmacion = confirm(`Estas seguro que deseas borrar este ${productoCarrito.nombre} de tu carrito?`);
+
+  if (confirmacion) {
+    productos = productos.filter((producto) => producto.nombre.toLowerCase() !== productoCarrito.nombre.toLowerCase());
+    mostrarProductos(productos);
+  } else {
+    alert("eliminacion cancelada");
+  }
+};
+
+const productoExistente = () => {
+  let nombreProducto = prompt("Ingrese nombre del producto");
+
+  let indice = productos.findIndex((producto) => producto.nombre.toLowerCase() === nombreProducto.toLowerCase());
+
+  if (indice === -1) {
+    return alert(`el producto ${nombreProducto} no existe`);
+  }
+
+  return productos[indice];
+};
+
 const productos = [
   {
     id: 1,
@@ -60,16 +106,15 @@ const productos = [
 ];
 
 let carrito = [];
-
-let seleccion = prompt("le gustaria comprar un producto si o no?");
+let seleccion = prompt("Desea comprar algun producto si o no?");
 
 while (seleccion !== "si" && seleccion != "no") {
   alert("si o no?");
-  seleccion = prompt("desea comprar algo si o no?");
+  seleccion = prompt("Desea comprar si o no?");
 }
 
 if (seleccion == "si") {
-  alert("ok, estos son nuestros productos");
+  alert("ok, estos son los productos disponibles");
   let losProductos = productos.map((producto) => producto.id + "- " + producto.nombre + " - " + "$" + producto.precio + " - " + "(" + producto.stock + " en stock)");
   alert(losProductos.join(" \n "));
 } else if (seleccion == "no") {
@@ -79,26 +124,25 @@ if (seleccion == "si") {
 while (seleccion != "no") {
   let producto = prompt("por favor elija el producto que desea agregar al carrito");
   let precio = 0;
-
   if (producto == "reishi" || producto == "cordyceps" || producto == "shiitake" || producto == "Melena de leon") {
     switch (producto) {
       case "reishi":
-        id = 1
+        id = 1;
         precio = 100;
         stock = 10;
         break;
       case "cordyceps":
-        id = 2
+        id = 2;
         precio = 150;
         stock = 10;
         break;
       case "shiitake":
-        id = 3
+        id = 3;
         precio = 120;
         stock = 10;
         break;
-      case "Melena de leon":
-        id = 4
+      case "melena de leon":
+        id = 4;
         precio = 180;
         stock = 10;
         break;
@@ -110,17 +154,39 @@ while (seleccion != "no") {
   } else {
     ("no tenemos ese producto");
   }
-
   seleccion = prompt("desea seguir comprando?");
 
   while (seleccion == "no") {
-    alert("gracias por la compra!");
-    carrito.forEach((carritoFinal) => {
-      console.log(`producto: ${carritoFinal.producto} total a pagar = $${carritoFinal.precio} pesos`);
-    });
-    break;
+    alert("Menú:\n1 - Mostrar productos\n2 - Eliminar un producto\n3 - Pagar");
+    let opcion = parseInt(prompt("Ingrese una opción"));
+
+    switch (opcion) {
+      case 1:
+        mostrarCarrito();
+        break;
+      case 2:
+        eliminarProducto();
+        break;
+      case 3:
+        pagarProductos();
+        break;
+      default:
+        alert("Ingrese una opcion correcta");
+    }
+
+    // carrito.forEach((carritoFinal) => {
+    //   console.log(`producto: ${carritoFinal.producto} total a pagar = $${carritoFinal.precio} pesos`);
+    // });
+    // break;
   }
 }
+
+alert("Nos vemos pronto!");
+
+// const comprarProducto = () => {
+//   let nombre = prompt("Ingrese el producto que desea")
+
+// }
 
 //Esto es para añadir en el futuro Eugenia! ↓↓
 
