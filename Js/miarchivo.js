9c396baac1bdd325e556a2045c93b48114c67dd8
Mackinlay Kevin <kemack83@gmail.com>
2023-08-10 23:30:44 -0300

    modificando codigo

--- Js/miarchivo.js
+++ Js/miarchivo.js
@@ -1,142 +1,152 @@
-alert("Bienvenido a la tienda FUNGI");
+let nombre = document.querySelector("#nombre");
+let btn = document.querySelector("#btn");
+let div = document.querySelector("#tarjeta");
+let container = document.querySelector("#container");
 
-class Producto {
-  constructor(id, nombre, precio, stock) {
-    this.id = id;
-    this.nombre = nombre;
-    this.precio = precio;
-    this.stock = stock;
-  }
-}
+// class Producto {
+//   constructor(id, nombre, precio, stock) {
+//     this.id = id;
+//     this.nombre = nombre;
+//     this.precio = precio;
+//     this.stock = stock;
+//   }
+// }
 
-const mostrarProductos = (productos) => {
-  console.clear();
-
-  const arrayProductos = productos.map((producto) => {
-    return {
-      id: producto.id,
-      nombre: producto.nombre,
-      precio: producto.precio,
-      stock: producto.stock,
-    };
-  });
-  console.log("Tienda FUNGI");
-  console.table(arrayProductos);
-};
-
-const productos = [new Producto(1, "reishi", 100, 3), new Producto(2, "cordyceps", 150, 5), new Producto(3, "shiitake", 120, 3), new Producto(4, "melena de leon", 200, 10)];
-
-mostrarProductos(productos);
+let productos = [{ nombre: Reishi, precio: 100, stock: 10 },
+  {nombre: Cordyceps, precio: 150, stock: 20},
+  {nombre: Shiitake, precio: 150, stock: 20},
+  {nombre: Melena de leon , precio: 200, stock: 15},
+  
+];
 
 
-const eliminarProducto = () => {
-  let idProducto = parseInt(prompt("Ingrese el código del item que desea borrar"));
 
-  let indice = productos.findIndex((producto) => producto.id == idProducto );
+// const mostrarProductos = (productos) => {
+//   console.clear();
 
-  if (indice === -1){
-    alert(`El producto con códigpo ${idProducto} no existe`);
-    return;
-  }
+//   const arrayProductos = productos.map((producto) => {
+//     return {
+//       id: producto.id,
+//       nombre: producto.nombre,
+//       precio: producto.precio,
+//       stock: producto.stock,
+//     };
+//   });
+//   console.log("Tienda FUNGI");
+//   console.table(arrayProductos);
+// };
 
-  const productoEliminar = productos[indice];
-  const confirmacion = confirm("Esta seguro que quiere eliminar este prducto?");
+// const productos = [new Producto(1, "reishi", 100, 3), new Producto(2, "cordyceps", 150, 5), new Producto(3, "shiitake", 120, 3), new Producto(4, "melena de leon", 200, 10)];
 
-  if (confirmacion) {
-    productos.splice(indice, 1);
-  console.log(`El producto con còdigo ${idProducto} fue eliminado.`);
-  } else {
-    alert("Eliminacion cancelada");
-  }
-};
+// mostrarProductos(productos);
 
-const salir = () => {
-  alert("Gracias por visitarnos!");
-};
+// const eliminarProducto = () => {
+//   let idProducto = parseInt(prompt("Ingrese el código del item que desea borrar"));
 
-let carrito = [];
+//   let indice = productos.findIndex((producto) => producto.id == idProducto );
 
-const totalCarrito = () => {
-  let total = 0;
-  carrito.forEach((producto) => {
-    total += producto.precio;
-  });
-  return total;
-};
+//   if (indice === -1){
+//     alert(`El producto con códigpo ${idProducto} no existe`);
+//     return;
+//   }
 
-const mostrarProductosCarrito = (productosCarrito) => {
-  console.clear();
+//   const productoEliminar = productos[indice];
+//   const confirmacion = confirm("Esta seguro que quiere eliminar este prducto?");
 
-  if (productosCarrito.length === 0) {
-    console.log("El carrito está vacío.");
-  } else {
-    console.log("Tienda Fungi");
-    console.log("Productos en el carrito:");
-    const arrayProductos = productosCarrito.map((producto) => {
-      return {
-        id: producto.id,
-        nombre: producto.producto,
-        precio: producto.precio,
-      };
-    });
-    console.table(arrayProductos);
-  }
-};
+//   if (confirmacion) {
+//     productos.splice(indice, 1);
+//   console.log(`El producto con còdigo ${idProducto} fue eliminado.`);
+//   } else {
+//     alert("Eliminacion cancelada");
+//   }
+// };
 
+// const salir = () => {
+//   alert("Gracias por visitarnos!");
+// };
 
-let seleccion = prompt("Desea comprar alguno de nuestros productos? (si/no) ");
+// let carrito = [];
 
-while (seleccion !== "si" && seleccion != "no") {
-  alert("si o no?");
-  seleccion = prompt("Desea comprar? si/no").toLowerCase();
-}
+// const totalCarrito = () => {
+//   let total = 0;
+//   carrito.forEach((producto) => {
+//     total += producto.precio;
+//   });
+//   return total;
+// };
 
-if (seleccion === "si") {
-  alert("ok, estos son los productos disponibles");
-  let losProductos = productos.map((producto) => producto.id + "- " + producto.nombre + " - " + "$" + producto.precio + " - " + "(" + producto.stock + " en stock)");
-  alert(losProductos.join(" \n "));
-} else if (seleccion === "no") {
-  alert("gracias por visitarnos!");
-}
+// const mostrarProductosCarrito = (productosCarrito) => {
+//   console.clear();
 
-let continuarComprando = "si";
+//   if (productosCarrito.length === 0) {
+//     console.log("El carrito está vacío.");
+//   } else {
+//     console.log("Tienda Fungi");
+//     console.log("Productos en el carrito:");
+//     const arrayProductos = productosCarrito.map((producto) => {
+//       return {
+//         id: producto.id,
+//         nombre: producto.producto,
+//         precio: producto.precio,
+//       };
+//     });
+//     console.table(arrayProductos);
+//   }
+// };
 
-while (continuarComprando === "si") {
-  let idProducto = parseInt(prompt("Por favor ingrese el código del producto que desea agregar al carrito"));
-  let producto = productos.find((producto) => producto.id === idProducto);
+// let seleccion = prompt("Desea comprar alguno de nuestros productos? (si/no) ");
 
-  if (!producto) {
-    alert(`El producto con código ${idProducto} no existe.`);
-    continuarComprando = prompt("¿Desea seguir comprando? (si/no)").toLowerCase();
-    continue;
-  }
+// while (seleccion !== "si" && seleccion != "no") {
+//   alert("si o no?");
+//   seleccion = prompt("Desea comprar? si/no").toLowerCase();
+// }
 
-  carrito.push({ id: producto.id, producto: producto.nombre, precio: producto.precio });
+// if (seleccion === "si") {
+//   alert("ok, estos son los productos disponibles");
+//   let losProductos = productos.map((producto) => producto.id + "- " + producto.nombre + " - " + "$" + producto.precio + " - " + "(" + producto.stock + " en stock)");
+//   alert(losProductos.join(" \n "));
+// } else if (seleccion === "no") {
+//   alert("gracias por visitarnos!");
+// }
 
-  continuarComprando = prompt("¿Desea seguir comprando? (si/no)").toLowerCase();
-} 
+// let continuarComprando = "si";
 
-// alert("Gracias por su compra!")
+// while (continuarComprando === "si") {
+//   let idProducto = parseInt(prompt("Por favor ingrese el código del producto que desea agregar al carrito"));
+//   let producto = productos.find((producto) => producto.id === idProducto);
 
-let opcionMenu;
-do {
-  alert("Menú:\n1 - Eliminar un producto\n2 - Total carrito\n3 - Salir");
-  opcionMenu = parseInt(prompt("Ingrese una opción"));
+//   if (!producto) {
+//     alert(`El producto con código ${idProducto} no existe.`);
+//     continuarComprando = prompt("¿Desea seguir comprando? (si/no)").toLowerCase();
+//     continue;
+//   }
 
-  switch (opcionMenu) {
-    case 1:
-      eliminarProducto();
-      break;
-    case 2:
-       console.log("Carrito:");
-       mostrarProductosCarrito(carrito);
-      console.log("Total a pagar: $" + totalCarrito());
-      break;
-    case 3:
-      salir();
-      break;
-    default:
-      alert("Ingrese una opcion correcta");
-      break;
-  }
-} while (opcionMenu  !== 3);
+//   carrito.push({ id: producto.id, producto: producto.nombre, precio: producto.precio });
+
+//   continuarComprando = prompt("¿Desea seguir comprando? (si/no)").toLowerCase();
+// }
+
+// // alert("Gracias por su compra!")
+
+// let opcionMenu;
+// do {
+//   alert("Menú:\n1 - Eliminar un producto\n2 - Total carrito\n3 - Salir");
+//   opcionMenu = parseInt(prompt("Ingrese una opción"));
+
+//   switch (opcionMenu) {
+//     case 1:
+//       eliminarProducto();
+//       break;
+//     case 2:
+//        console.log("Carrito:");
+//        mostrarProductosCarrito(carrito);
+//       console.log("Total a pagar: $" + totalCarrito());
+//       break;
+//     case 3:
+//       salir();
+//       break;
+//     default:
+//       alert("Ingrese una opcion correcta");
+//       break;
+//   }
+// } while (opcionMenu  !== 3);
