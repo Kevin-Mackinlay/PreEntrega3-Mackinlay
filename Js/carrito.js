567207c7e3236976d4da87b564cb930a0d9d1913
Mackinlay Kevin <kemack83@gmail.com>
2023-08-20 00:13:46 -0300

    modificando todo el html y el js

--- Js/carrito.js
+++ Js/carrito.js
@@ -1,134 +1,239 @@
-//llamamos elementos
-const productosEl = document.querySelector(".productos");
-const carritoItemsEl = document.querySelector(".cart-items");
-const carritoItemel = document.querySelector(".cart-item");
+const productsEl = document.querySelector(".products");
+const cartItemsEl = document.getElementById("cartItems");
+const cartItemEl = document.querySelector(".cart");
 const subtotalEl = document.querySelector(".subtotal");
+const totalPriceEl = document.querySelector("#totalPrice");
+const clearCartEl = document.getElementById("clear");
 
-//mostramos productos
-const mostrarProductos = () => {
-  productosEl.innerHTML = " ";
-  productos.forEach((producto, index) => {
-    let productoBox = document.createElement("div");
-    productoBox.innerHTML = ` 
-      <div class="card" id="tarjeta" style="width: 18rem">
-        <h4>${producto.nombre}</h4>
-        <h6>Precio:${producto.precio}</h6>
-        <p>Stock: ${producto.stock}</p>
-        <img src="${producto.imgSrc}" alt="">
-      </div>
-      `;
-    productosEl.appendChild(productoBox);
+const renderProducts = () => {
+  productsEl.innerHTML = " ";
+  products.forEach((product, index) => {
+    let productBox = document.createElement("div");
+    productBox.innerHTML = `
+             <div class="product medium-4 columns" data-name=${product.name} data-price= ${product.price} data-id=${product.id}>
+                <img src= ${product.imgSrc} alt=${product.name} />
+                <h3>${product.name}</h3>
+                <input type="text" class="count" value="${product.stock}" />
+                <button class="tiny" ${product.stock <= 0 ? "disabled" : ""}>Add to cart</button>
+            </div>`;
+    productsEl.appendChild(productBox);
 
-    let btnAgregar = document.createElement("button");
-    btnAgregar.innerHTML = `
-     
-        <button class=" btn btn-success"> Agregar </button>
-      `;
-    productoBox.appendChild(btnAgregar);
-
-
-    btnAgregar.onclick = () => agregarCarrito(index);
-
-    
-
-    btnAgregar.addEventListener("click", () => {
-      swal.fire({
-        title: "Genial",
-        text: "Haz agregado estre producto al carrito",
-        icon: "success",
-        confirmButtonText: "ok",
-      });
-    });
-
-    
-  
-
-    let btnEliminar = document.createElement("button");
-    btnEliminar.innerHTML = `
-     
-        <button class=" btn btn-danger"> Eliminar </button>
-      `;
-    productoBox.appendChild(btnEliminar);
-
-    btnEliminar.onclick = () => eliminarItemCarrito(index);
-
-    btnEliminar.addEventListener("click", () => {
+    let btnAdd = productBox.querySelector("button"); // Select the button element
+    btnAdd.onclick = () => addToCart(product.id);
+    // Add click event listener to the button
+    btnAdd.addEventListener("click", () => {
+      if (product.stock <= 0) {
+        alert("This product is out of stock.");
+        return;
+      }
       Toastify({
-        text: "Producto eliminado!",
+        text: "Added product!",
         duration: 2000,
-        gravity: "top",
+        gravity: "center",
         position: "right",
         style: {
-          background: "linear-gradient(to right, #ed754a,  #e9a187 ",
+          background: "linear-gradient(to right, #5bce51 ,  #bcedb8  ",
         },
       }).showToast();
-    })
+    });
   });
 };
 
-mostrarProductos();
+renderProducts();
 
-//carrito array
-let carrito;
+clearCartEl.addEventListener("click", clear);
+const clearCart = () => {
+  // Clear the cart array
+  cart = [];
+  // Clear localStorage
+  localStorage.removeItem("cart");
+  // Update the cart display
+  updateCart();
+};
 
-if (localStorage.getItem("carrito") === null) {
-  carrito = [];
+// array cart
+let cart;
+
+if (localStorage.getItem("cart") === null) {
+  cart = [];
 } else {
-  carrito = JSON.parse(localStorage.getItem("carrito"));
+  cart = JSON.parse(localStorage.getItem("cart"));
 }
 
-// agregar funcion
+const countRemainingStock = () => {
+  products.forEach((product) => {
+    let remainingStock = product.stock;
 
-function agregarCarrito(id) {
-  //chequeamos si el id del producto existe
-  if (carrito.some((item) => item.id === id)) {
-    alert("Este producto ya esta en el carrito!");
+    cart.forEach((item) => {
+      if (item.id === product.id) {
+        remainingStock -= item.numberOfUnits;
+      }
+    });
+
+    product.remainingStock = remainingStock;
+  });
+};
+
+const updateCart = () => {
+  countRemainingStock();
+  renderTotalPrice();
+};
+
+const addToCart = (id) => {
+  console.log("addToCart called");
+  const item = products.find((product) => product.id === id);
+  console.log("Item stock:", item.stock);
+  if (item.stock <= 0) {
+    alert("this product is out of stock.");
   } else {
-    const item = productos.find((producto) => producto.id === id);
-    carrito.push({
+    cart.push({
       ...item,
       numberOfUnits: 1,
     });
-    localStorage.setItem("carrito", JSON.stringify(carrito));
+    localStorage.setItem("cart", JSON.stringify(cart));
+    console.log("Cart updated:", cart);
   }
-  actualizarCarrito();
-}
+  updateCart();
+  countRemainingStock();
+};
 
-// actualizar carrito
+const renderTotalPrice = () => {
+  let totalPrice = 0;
 
-function actualizarCarrito() {
-  mostrarCarritoItems();
-  mostrarSubtotal();
-}
-
-//calcular subtotal
-
-function mostrarSubtotal() {
-  let precioTotal = 0;
-
-  carrito.forEach((item) => {
-    precioTotal += item.precio;
+  cart.forEach((item) => {
+    totalPrice += item.price * item.numberOfUnits;
   });
-  subtotalEl.innerHTML = `Subtotal: $${precioTotal}`;
-}
+  console.log("TOTAL Price:", totalPrice);
+  totalPriceEl.textContent = `${totalPrice} $`;
+};
 
-//funcion mostrar carrito
-function mostrarCarritoItems() {
-  carritoItemsEl.innerHTML = ""; // borra el elemento del carrito
-  carrito.forEach((item) => {
-    carritoItemsEl.innerHTML += `
-      <div class="cart-item"  style="width: 10rem">
-              <h4>${item.nombre}</h4>
-              <h6>Precio:${item.precio}</h6>
-      </div>
-    
-`;
-  });
-}
+renderTotalPrice();
 
-//funcion Eliminar
-function eliminarItemCarrito(id) {
-  carrito = carrito.filter((item) => item.id !== productos[id].id);
+// //llamamos elementos
+// const productosEl = document.querySelector(".productos");
+// const carritoItemsEl = document.querySelector(".cart-items");
+// const carritoItemel = document.querySelector(".cart-item");
+// const subtotalEl = document.querySelector(".subtotal");
 
-  actualizarCarrito();
-}
+// //mostramos productos
+// const mostrarProductos = () => {
+//   productosEl.innerHTML = " ";
+//   productos.forEach((producto, index) => {
+//     let productoBox = document.createElement("div");
+//     productoBox.innerHTML = `
+//       <div class="card" id="tarjeta" style="width: 18rem">
+//         <h4>${producto.nombre}</h4>
+//         <h6>Precio:${producto.precio}</h6>
+//         <p>Stock: ${producto.stock}</p>
+//         <img src="${producto.imgSrc}" alt="">
+//       </div>
+//       `;
+//     productosEl.appendChild(productoBox);
+
+//     let btnAgregar = document.createElement("button");
+//     btnAgregar.innerHTML = `
+
+//         <button class=" btn btn-success"> Agregar </button>
+//       `;
+//     productoBox.appendChild(btnAgregar);
+
+//     btnAgregar.onclick = () => agregarCarrito(index);
+
+//     btnAgregar.addEventListener("click", () => {
+//       swal.fire({
+//         title: "Genial",
+//         text: "Haz agregado estre producto al carrito",
+//         icon: "success",
+//         confirmButtonText: "ok",
+//       });
+//     });
+
+//     let btnEliminar = document.createElement("button");
+//     btnEliminar.innerHTML = `
+
+//         <button class=" btn btn-danger"> Eliminar </button>
+//       `;
+//     productoBox.appendChild(btnEliminar);
+
+//     btnEliminar.onclick = () => eliminarItemCarrito(index);
+
+//     btnEliminar.addEventListener("click", () => {
+//       Toastify({
+//         text: "Producto eliminado!",
+//         duration: 2000,
+//         gravity: "top",
+//         position: "right",
+//         style: {
+//           background: "linear-gradient(to right, #ed754a,  #e9a187 ",
+//         },
+//       }).showToast();
+//     })
+//   });
+// };
+
+// mostrarProductos();
+
+// //carrito array
+// let carrito;
+
+// if (localStorage.getItem("carrito") === null) {
+//   carrito = [];
+// } else {
+//   carrito = JSON.parse(localStorage.getItem("carrito"));
+// }
+
+// // agregar funcion
+
+// function agregarCarrito(id) {
+//   //chequeamos si el id del producto existe
+//   if (carrito.some((item) => item.id === id)) {
+//     alert("Este producto ya esta en el carrito!");
+//   } else {
+//     const item = productos.find((producto) => producto.id === id);
+//     carrito.push({
+//       ...item,
+//       numberOfUnits: 1,
+//     });
+//     localStorage.setItem("carrito", JSON.stringify(carrito));
+//   }
+//   actualizarCarrito();
+// }
+
+// // actualizar carrito
+
+// function actualizarCarrito() {
+//   mostrarCarritoItems();
+//   mostrarSubtotal();
+// }
+
+// //calcular subtotal
+
+// function mostrarSubtotal() {
+//   let precioTotal = 0;
+
+//   carrito.forEach((item) => {
+//     precioTotal += item.precio;
+//   });
+//   subtotalEl.innerHTML = `Subtotal: $${precioTotal}`;
+// }
+
+// //funcion mostrar carrito
+// function mostrarCarritoItems() {
+//   carritoItemsEl.innerHTML = ""; // borra el elemento del carrito
+//   carrito.forEach((item) => {
+//     carritoItemsEl.innerHTML += `
+//       <div class="cart-item"  style="width: 10rem">
+//               <h4>${item.nombre}</h4>
+//               <h6>Precio:${item.precio}</h6>
+//       </div>
+
+// `;
+//   });
+// }
+
+// //funcion Eliminar
+// function eliminarItemCarrito(id) {
+//   carrito = carrito.filter((item) => item.id !== productos[id].id);
+
+//   actualizarCarrito();
+// }
--- Js/productos.js
+++ Js/productos.js
@@ -1,6 +1,9 @@
-let productos = [
-  { id: 0, nombre: "Reishi", precio: 100, stock: 10, imgSrc: "images/shop4.png" },
-  { id: 1, nombre: "Cordyceps", precio: 150, stock: 20, imgSrc: "images/shop9.png" },
-  { id: 2, nombre: "Shiitake", precio: 150, stock: 20, imgSrc: "images/shop8.png" },
-  { id: 3, nombre: "Melena", precio: 200, stock: 5, imgSrc: "images/shop7.png" },
+
+let products = [
+  { id: 0, name: "Reishi", price: 100, stock: 10, imgSrc: "images/shop9.png" },
+  { id: 1, name: "Cordyceps", price: 150, stock: 20, imgSrc: "images/shop9.png" },
+  { id: 2, name: "Shiitake", price: 150, stock: 20, imgSrc: "images/shop8.png" },
+  { id: 3, name: "Melena", price: 200, stock: 5, imgSrc: "images/shop8.png" },
+  { id: 4, name: "Cordyceps", price: 150, stock: 20, imgSrc: "images/shop9.png" },
+  { id: 5, name: "Cordyceps", price: 150, stock: 20, imgSrc: "images/shop9.png" },
 ];
